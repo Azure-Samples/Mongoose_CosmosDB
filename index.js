86a4a97850f67ac58583203556674e97e0665493
Sneha Gunda <sngun@microsoft.com>
2019-11-18 23:04:29 -0800

    Updating environment variables

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 var env = require('dotenv').config();
 
-console.log(process.env.COSMOSDB_CONNSTR+process.env.COSMOSDB_DBNAME);
- mongoose.connect("mongodb://"+process.env.DB_HOST+":"+process.env.DB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
+ mongoose.connect("mongodb://"+process.env.COSMOSDB_HOST+":"+process.env.COSMOSDB_PORT+"/"+process.env.COSMOSDB_DBNAME+"?ssl=true&replicaSet=globaldb", {
       auth: {
         user: process.env.COSMODDB_USER,
         password: process.env.COSMOSDB_PASSWORD
